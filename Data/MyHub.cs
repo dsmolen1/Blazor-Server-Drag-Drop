b426ce2dd91400c4ad3d83d620f749a7147f89c5
don smolen <dsmolen@comcast.net>
2020-07-08 13:07:07 -0400

    Add project files.

---
+++ Data/MyHub.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDragDrop.Data
+{
+    public class MyHub : Hub
+    {
+        // this list holds the ids of the cards in the current order - this is the state of the app
+        private static List<string> cards = new List<string>();
+
+        // when this method is called, the app is initialized
+        public async Task State()
+        {
+            cards = new List<string>() { "AS", "KH", "QC", "JD", "TS" };
+            await Clients.Caller.SendAsync(MessageType.STATE, new StateMessage() { Cards = cards });
+        }
+
+        // this method changes the state of the app to reflect the drag/drop and then sends the new state to the client
+        public async Task CardMoved(string src, string dest)
+        {
+            cards.Remove(src);
+            cards.Insert(cards.IndexOf(dest), src);
+            await Clients.Caller.SendAsync(MessageType.STATE, new StateMessage() { Cards = cards });
+        }
+    }
+
+    // using these strings is preferable to hard-coding the method names in the calls
+    public static class MessageType
+    {
+        public static string CARD_MOVED = "CardMoved";
+        public static string STATE = "State";
+    }
+
+    // this message conveys the state of the game to the browser
+    public class StateMessage
+    {
+        public List<string> Cards { get; set; }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿/* drag drop support
+ The idea is to let all the drag-drop operations take place in the browser, but not modify the DOM.
+ Then call a C# method which handles the rearrangement and rerendering.
+*/
+var DotNetHelper;
+
+// initializes a DotNetObjectReference to enable calling C# from Javascript
+// called by Index.razor
+window.setDotNetObjRef = (dotNetHelper) => {
+    DotNetHelper = dotNetHelper;
+}
+
+// when drag starts, save the id of the object being dragged
+function dragstart_handler(ev) {
+    ev.dataTransfer.setData("text/plain", ev.target.id);
+    ev.dataTransfer.dropEffect = "copy";
+}
+
+// get the source and destination of the move and invoke the CardMoved method in Index.razor
+function drop_handler(ev) {
+    ev.preventDefault();
+    var source = ev.dataTransfer.getData("text/plain");
+    var dest = ev.target.id;
+    if (source != dest) DotNetHelper.invokeMethodAsync("CardMoved", source, dest);
+}
+
+function dragover_handler(ev) {
+    ev.preventDefault();
+}
